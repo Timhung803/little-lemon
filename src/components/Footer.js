52220ac3a23e9e5152604c0e0b0b65a16ded5ebf
Timhung803 <timhkt@gmail.com>
2023-04-01 03:43:27 +1300

    Added the necessary HTML elements to the footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,55 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src="./Images/footerLogo.png" alt="" />
+      <nav>
+        <h3>Little Lemon</h3>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Menu</a>
+          </li>
+          <li>
+            <a href="">Reservations</a>
+          </li>
+          <li>
+            <a href="">Order Online</a>
+          </li>
+          <li>
+            <a href="">Login</a>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <h3>Contact</h3>
+        <ul>
+          <li>Address</li>
+          <li>Email</li>
+          <li>Phome:</li>
+          <li>Opening Hour</li>
+          <ul>
+            <li>Monday - Friday: 9:00 - 22:00</li>
+            <li>Saturday - Sunday: 10:00 - 24:00</li>
+          </ul>
+        </ul>
+      </nav>
+      <nav>
+        <h3>Social Media</h3>
+        <ul>
+          <li>Facebook</li>
+          <li>Pinterest</li>
+          <li>Titter</li>
+        </ul>
+      </nav>
+    </footer>
+  );
 }
 
 export default Footer;
