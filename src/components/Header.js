31644553e72c1b0dece1df1750eeeb757d70e0d5
Timhung803 <timhkt@gmail.com>
2023-04-05 14:30:49 +1200

    Styled the hero section.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Header.css";
 
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <article>
+        <section>
+          <h1>Little Lemon</h1>
+          <h3>Chicage</h3>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button>Reserve a table</button>
+        </section>
+        <section>
+          <img src="./Images/restauranfood.jpg" alt="" />
+        </section>
+      </article>
+    </header>
+  );
 }
 
 export default Header;
