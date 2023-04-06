530b314317c38ba89a9ecd82238bb3db6d5100a7
Timhung803 <timhkt@gmail.com>
2023-04-07 02:49:49 +1200

    Styled footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,54 +1,59 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <footer>
-      <img src="./Images/footerLogo.png" alt="" />
-      <nav>
-        <h3>Little Lemon</h3>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Menu</a>
-          </li>
-          <li>
-            <a href="">Reservations</a>
-          </li>
-          <li>
-            <a href="">Order Online</a>
-          </li>
-          <li>
-            <a href="">Login</a>
-          </li>
-        </ul>
-      </nav>
-      <nav>
-        <h3>Contact</h3>
-        <ul>
-          <li>Address</li>
-          <li>Email</li>
-          <li>Phome:</li>
-          <li>Opening Hour</li>
+    <footerbackground>
+      <footer>
+        <nav>
+          <img src="./Images/footerLogo.png" alt="" />
+        </nav>
+        <nav>
+          <h3>Little Lemon</h3>
           <ul>
-            <li>Monday - Friday: 9:00 - 22:00</li>
-            <li>Saturday - Sunday: 10:00 - 24:00</li>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Menu</a>
+            </li>
+            <li>
+              <a href="">Reservations</a>
+            </li>
+            <li>
+              <a href="">Order Online</a>
+            </li>
+            <li>
+              <a href="">Login</a>
+            </li>
           </ul>
-        </ul>
-      </nav>
-      <nav>
-        <h3>Social Media</h3>
-        <ul>
-          <li>Facebook</li>
-          <li>Pinterest</li>
-          <li>Titter</li>
-        </ul>
-      </nav>
-    </footer>
+        </nav>
+        <nav>
+          <h3>Contact</h3>
+          <ul>
+            <li>Address: 123 ABC Street</li>
+            <li>Email: info@littlelemon.com</li>
+            <li>Phome: 012 345 6789</li>
+            <li id="hour">Opening Hours:</li>
+            <ul id="date">
+              <li>Mon - Fri: 9:00 - 22:00</li>
+              <li>Sat - Sun: 10:00 - 24:00</li>
+            </ul>
+          </ul>
+        </nav>
+        <nav>
+          <h3>Social Media</h3>
+          <ul>
+            <li>Facebook</li>
+            <li>Pinterest</li>
+            <li>Titter</li>
+          </ul>
+        </nav>
+      </footer>
+    </footerbackground>
   );
 }
 
