1751f70e352a56b1f9da3eeaeb1d71d5b45d7069
Timhung803 <timhkt@gmail.com>
2023-04-06 16:01:39 +1200

    Styled the main component and installed material icons.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,72 @@
 import React from "react";
+import "./Main.css";
+import DeliveryDiningIcon from "@mui/icons-material/DeliveryDining";
 
 function Main() {
-  return <main>Main</main>;
+  return (
+    <main>
+      <section>
+        <h1>This week Specials!</h1>
+        <button>Online Menu</button>
+      </section>
+      <section>
+        <cards>
+          <img src="./Images/greek salad.jpg" alt="greek salad" />
+          <dishname>
+            <h3>Greek Salad</h3>
+            <p>$12.95</p>
+          </dishname>
+          <description>
+            <p>
+              The famous Greek salad of crispy lettuce, peppers, olives and our
+              Chicago style feta cheese, garnished with crunchy garlic and
+              rosemary croutons.
+            </p>
+            <section>
+              <p>Online & delivery</p>
+              <DeliveryDiningIcon />
+            </section>
+          </description>
+        </cards>
+        <cards>
+          <img src="./Images/greek salad.jpg" alt="greek salad" />
+          <dishname>
+            <h3>Greek Salad</h3>
+            <p>$12.95</p>
+          </dishname>
+          <description>
+            <p>
+              The famous Greek salad of crispy lettuce, peppers, olives and our
+              Chicago style feta cheese, garnished with crunchy garlic and
+              rosemary croutons.
+            </p>
+            <section>
+              <p>Online & delivery</p>
+              <DeliveryDiningIcon />
+            </section>
+          </description>
+        </cards>
+        <cards>
+          <img src="./Images/greek salad.jpg" alt="greek salad" />
+          <dishname>
+            <h3>Greek Salad</h3>
+            <p>$12.95</p>
+          </dishname>
+          <description>
+            <p>
+              The famous Greek salad of crispy lettuce, peppers, olives and our
+              Chicago style feta cheese, garnished with crunchy garlic and
+              rosemary croutons.
+            </p>
+            <section>
+              <p>Online & delivery</p>
+              <DeliveryDiningIcon />
+            </section>
+          </description>
+        </cards>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
