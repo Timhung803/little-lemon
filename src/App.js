44144182403fd15bff14f1946e9dea91aa01e111
Timhung803 <timhkt@gmail.com>
2023-04-09 02:02:57 +1200

    Set up the Navigation. created links for homepage and booking page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import BookingPage from "./pages/BookingPage";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
@@ -6,12 +9,14 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
-      <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/booking" element={<BookingPage />}></Route>
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
